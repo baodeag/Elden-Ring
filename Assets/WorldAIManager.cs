3f9500e3f19adbc12d4a6656dd8aa3695e26a642
baodeag <tranvietbao0907@gmail.com>
2025-09-30 15:11:02 +0700

    AI Characters p1

---
+++ Assets/WorldAIManager.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+using Unity.Netcode;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace baodeag
+{
+    public class WorldAIManager : MonoBehaviour
+    {
+        public static WorldAIManager instance;
+
+        [Header("Debug")]
+        [SerializeField] bool despawnCharacters = false;
+        [SerializeField] bool respawnCharacters = false;
+
+        [Header("Characters")]
+        [SerializeField] GameObject[] aiCharacters;
+        [SerializeField] List<GameObject> spawnedInCharacters;
+
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        private void Start()
+        {
+            if (NetworkManager.Singleton.IsServer)
+            {
+                StartCoroutine(WaitForSceneToLoadThenSpawnCharacters());
+            }
+        }
+
+        private void Update()
+        {
+            if (respawnCharacters)
+            {
+                respawnCharacters = false;
+                SpawnAllCharacters();
+            }
+            if (despawnCharacters)
+            {
+                despawnCharacters = false;
+                DespawnAllCharacters();
+            }
+
+        }
+
+        private IEnumerator WaitForSceneToLoadThenSpawnCharacters()
+        {
+            while (!SceneManager.GetActiveScene().isLoaded)
+            {
+                yield return null;
+            }
+
+            SpawnAllCharacters();
+        }
+
+        private void SpawnAllCharacters()
+        {
+            foreach (var character in aiCharacters)
+            {
+                GameObject instantiatedCharacter = Instantiate(character);
+                instantiatedCharacter.GetComponent<NetworkObject>().Spawn();
+                spawnedInCharacters.Add(instantiatedCharacter);
+            }
+        }
+
+        private void DespawnAllCharacters()
+        {
+            foreach (var character in spawnedInCharacters)
+            {
+                character.GetComponent<NetworkObject>().Despawn();
+            }
+        }
+
+        private void DisableAllCharacters()
+        {
+
+        }
+    }
+}
