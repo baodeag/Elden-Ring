12d24754b839691dc3bb23735baeefcc0461a7ae
baodeag <tranvietbao0907@gmail.com>
2026-05-30 17:46:38 +0700

    [Build] Repair Stuck Loading p1

--- Assets/Game/Scripts/Character/AI Character/AIPatrolPath.cs
+++ Assets/Game/Scripts/Character/AI Character/AIPatrolPath.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System.Collections;
 
 namespace baodeag
 {
@@ -7,15 +8,56 @@ namespace baodeag
     {
         public int patrolPathID = 0;
         public List<Vector3> patrolPoints = new List<Vector3>();
+        private Coroutine registerCoroutine;
 
         private void Awake()
         {
+            CachePatrolPoints();
+        }
+
+        private void OnEnable()
+        {
+            TryRegisterWithWorldAIManager();
+        }
+
+        private void OnDisable()
+        {
+            if (registerCoroutine != null)
+            {
+                StopCoroutine(registerCoroutine);
+                registerCoroutine = null;
+            }
+        }
+
+        private void CachePatrolPoints()
+        {
+            patrolPoints.Clear();
+
             for (int i = 0; i < transform.childCount; i++)
             {
                 patrolPoints.Add(transform.GetChild(i).position);
             }
+        }
+
+        private void TryRegisterWithWorldAIManager()
+        {
+            if (WorldAIManager.instance != null)
+            {
+                WorldAIManager.instance.AddPatrolPathToList(this);
+                return;
+            }
+
+            if (registerCoroutine == null)
+                registerCoroutine = StartCoroutine(RegisterWhenWorldAIManagerIsReady());
+        }
+
+        private IEnumerator RegisterWhenWorldAIManagerIsReady()
+        {
+            while (WorldAIManager.instance == null)
+                yield return null;
 
             WorldAIManager.instance.AddPatrolPathToList(this);
+            registerCoroutine = null;
         }
     }
 }
